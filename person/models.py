11432641d5ee11741b75d25bcf2ab6e036ee57c1
Jay Graves <jgraves@dev20lt.(none)>
2008-08-26 17:25:49 -0600

    Started on the models.

--- person/models.py
+++ person/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    firstName = models.CharField(max_length=32)
+    lastName = models.CharField(max_length=32)
+    companyName = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    bangledeshiTitle = models.CharField(max_length=200, blank=True)
+    
+    def __unicode__(self):
+        return u"%s %s - %s" (self.firstName, self.lastName, self.title) 
+
+class PhoneNumber(models.Model):
+    number = models.CharField(max_length=32)
+    label = models.CharField(max_length=32)
+    person = models.ManyToManyField(Person)
+
+    def __unicode__(self):
+        return u"%s %s" % (self.label, self.number)
+
+class EmailAddress(models.Model):
+    emailAddress = models.EmailField()
+    label = models.CharField(max_length=32)
+    person = models.ManyToManyField(Person)
+    
+    def __unicode__(self):
+        return u"%s %s" % (self.label, self.emailAddress)
