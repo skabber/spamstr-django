831a3829d654e8594ce2f4b51e5386c50f19c4af
Jay Graves <jay@skabber.com>
2008-08-28 08:07:48 -0600

    added the admin.py file

---
+++ person/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from spamstr.person.models import Person, PhoneNumber, EmailAddress
+
+class PhoneNumberInline(admin.StackedInline):
+    model = PhoneNumber
+    extra = 2
+
+class EmailAddressInline(admin.StackedInline):
+    model = EmailAddress
+    extra = 2
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('firstName', 'lastName', 'title')
+    inlines = [PhoneNumberInline, EmailAddressInline]
+
+admin.site.register(Person, PersonAdmin)
